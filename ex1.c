37e137665a7b9488ee5a746fa7a49ab31c3bd100
AhmetElas-lab <ahmetelas14@gmail.com>
2025-10-22 00:26:15 +0300

    Implement number guessing game in ex1.c
    Added a simple number guessing game with input validation.

--- ex1.c
+++ ex1.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int number;
+    printf("HI! let's guess a number between 1 and 5.\n");
+    scanf("%d", &number);
+  
+    if(number < 1 || number > 5)
+    {
+        //hata kodunu harf için ayrı yapmadım zaten 1 ila 5 arasında yazılabilicek karakter yok.
+        printf("PLEASE WRITE NUMBER BETWEEN 1 AND 5\n");
+        return 1;
+    }
+    else if (number == 3)
+    {
+        printf("Congratulations! You guessed the number correctly.\n");
+        return 0;
+    }
+    else
+    {
+        printf("Sorry, that's not the number I was thinking of.\n");
+        return 1;
+    }
+}
 
