9e73aab018ca1078e00fa757f2cbad35f542df66
AhmetElas-lab <ahmetelas14@gmail.com>
2025-10-24 14:08:15 +0300

    Add pet interaction feature to ex3.c

--- ex3.c
+++ ex3.c
@@ -1,2 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
-
+int main (void)
+{
+    bool hasPet;
+    char pet[20];
+    printf("Do you have any pet? (1 for yes, 0 for no)\n");
+    scanf("%d", &hasPet);
+    if (hasPet)
+    {
+        printf("What kind of pet you have?\n");
+        scanf("%s", pet);
+        
+        if (strcmp(pet, "cat") == 0)
+        {
+            printf("Meow!\n");
+        } 
+        else if (strcmp(pet, "dog") == 0) 
+        {
+            printf("Woof! Woof!\n");
+        } 
+        else
+        {
+            printf("Hmm, I only know the sounds for cats and dogs!\n");
+        }
+    }
+    else
+    {
+        printf("You do not have a pet.\n");
+    }
+}
