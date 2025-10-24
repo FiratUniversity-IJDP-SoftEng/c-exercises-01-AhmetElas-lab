348219144fa2e3e678135501a14984bc91138057
AhmetElas-lab <ahmetelas14@gmail.com>
2025-10-24 22:37:25 +0300

    Add greeting function to say hello to user

--- ex5.c
+++ ex5.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+void sayHello();
+int main(void)
+{
+    sayHello();
+    return 0;
+}
+
+void sayHello()
+{
+    char name[50];
+    printf("HI! What's your name?\n");
+    scanf("%s", name);
+    printf("Hello! %s\n", name);
+}
