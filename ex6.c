f5cc9fd4830a3b9981608ec601ed2e7df76dafa7
AhmetElas-lab <ahmetelas14@gmail.com>
2025-10-27 12:07:16 +0300

    Update ex6.c

--- ex6.c
+++ ex6.c
@@ -1,1 +1,66 @@
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#define MAX_LENGTH 3000
+
+
+
+int main(void)
+{
+    char text[MAX_LENGTH];
+
+    printf("Welcome to Grade calculation machine. \n");
+    printf("Plesae, Enter your essay: \n");
+    
+    if (fgets(text, MAX_LENGTH, stdin) == NULL) 
+    {
+        printf("Please enter an essay next time: \n");
+        return 1; 
+    }
+
+
+    double letter = 0;
+    double word = 1;
+    double sentence = 0;
+    double n = strlen(text);
+    
+    for (int i = 0; i < n; i++) 
+    {
+        char character = text[i];
+
+        if (isalpha(character)) //to count letter
+        {
+            letter++;
+        }
+        else if (isspace(character)) //to count word
+        {
+            word++;
+        }
+        else if (character == '?' || character == '!' || character == '.') //to count sentence
+        {
+            sentence++;
+        }
+    } 
+
+    double L = (double) letter / word * 100;  // calculation part
+    double S = (double) sentence / word * 100;
+    double index =0.0588 * L - 0.296 * S - 15.8;
+
+    int result = round(index);
+    
+    if (result >= 16) // the last part
+    {
+        printf("Grade 16+ \n");
+    }
+    else if (result < 1)
+    {
+        printf("before grade 1 \n");
+    }
+    else 
+    {
+        printf("Grade %i\n", result);
+    }
+    return 0;
+}
